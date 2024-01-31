8be6dde36d998b50e2cc2526989eb972cf64d9d9
Fuyang Liu <liufuyang@users.noreply.github.com>
2024-01-31 10:40:56 +0100

    add gradle config

---
+++ p1/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+  application
+}
+
+group = "com.example"
+version = "0.0.1"
+
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("src"))
+        }
+        resources {
+            setSrcDirs(listOf("src"))
+        }
+    }
+}
+
+application {
+    mainClass.set("p1.P1")
+}
